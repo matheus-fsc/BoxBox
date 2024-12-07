769f506a459db655e894903dfd77670a83562424
Matheus Coelho <86948040+matheus-fsc@users.noreply.github.com>
2024-12-07 18:53:51 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import PhaserGame from './PhaserGame';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import PhaserGame from "./PhaserGame";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./Home/Home";
+import Menu from "./Menu/Menu";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <PhaserGame />
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
