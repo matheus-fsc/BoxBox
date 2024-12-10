084af26f55bcc9e056ef4d719fa98679c778caa2
Matheus Coelho <matheusvgp29@gmail.com>
2024-12-10 11:41:34 -0300

    Refactor Home component layout and styles for improved structure and responsiveness

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,16 +3,33 @@ import './Home.css'
 
 export default function Home() {
   return (
-    <>
+    <div className='container'>
       <div className='Logo'>
           <h1 className='LogoTex'>BoxBox.io</h1>
       </div>
+        
       <div className='Home'>
-    teste
+        <div className='HomeContent'>
+              <div className='InputContainer'>
+                <input type='text' placeholder='Digite seu nome' className='HomeInput'></input>
+                <div className='RegionContainer'>
+                  <select className='HomeInputRegion'>
+                    <option value=''>Escolha sua região</option>
+                    <option value='norte'>Norte</option>
+                    <option value='nordeste'>Nordeste</option>
+                    <option value='centro-oeste'>Centro-Oeste</option>
+                    <option value='sudeste'>Sudeste</option>
+                    <option value='sul'>Sul</option>
+                  </select>
+                </div>
+              </div>
+              <button className='HomeButton'>Jogar</button>
+              <p className='HomeText'>Registrar</p>
+        </div>
       </div>
       <footer>
           <p>BoxBox - 2021</p>
       </footer>
-    </>
+    </div>
   )
 }
