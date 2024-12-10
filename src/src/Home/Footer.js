c9b75080398c169e86064c90f499c78a067e6343
Matheus Coelho <matheusvgp29@gmail.com>
2024-12-10 12:53:59 -0300

    Add SettingsMenu component and update Footer: implement settings functionality with toggle and styling

--- src/src/Home/Footer.js
+++ src/src/Home/Footer.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Style/Footer.css';
+import SettingsIcon from './HomeAssets/Settings.svg';
+import SettingsMenu from './SettingsMenu';
 
 export default function Footer() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <footer>
-      <p>BoxBox - 2021</p>
+      <img
+        src={SettingsIcon}
+        alt="Settings"
+        className="settings-icon"
+        onClick={toggleMenu}
+      />
+      <SettingsMenu isVisible={isMenuVisible} toggleMenu={toggleMenu} />
     </footer>
   );
 }
